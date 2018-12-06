275165b748f24704a4efe4795aacd6b36ff01ec1
Joseph Pham <j.pham017@gmail.com>
2018-12-06 12:37:47 -0500

    added basic admin-auth

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['USERNAME'].to_s , :password => ENV['PASSWORD'].to_s
 
   def index
     @categories = Category.order(name: :desc).all
